46837d9a1007bdee85d686662e8294fe8d81cccc
Keara G <kgilliga@nd.edu>
2020-10-22 00:03:23 -0400

    Debugging but Progressing...

--- Exercise8.R
+++ Exercise8.R
@@ -2,23 +2,15 @@ setwd ("/Users/kearagilligan/Desktop/Biocomputing/Biocomputing2020_Tutorial10/")
 #Question 1- cumulative score over time graph
 
 #Question 2- Guess my number
+print ("I'm thinking of a number 1-100")
+maxGuesses= 10
 x=sample(1:100, 1)
 guess <- function(y)
+for (i in 1:maxGuesses)
   {if (y > x){
   print("lower")}
   else if (y < x)
   {print("higher")}
   else {print("correct!")}}
-guess (x,50)
-guess (x,22)
+readline(prompt= "Guess:")
 
- 
-
-
-multiplyByTwo<-function(x){
-  if(is.numeric(x)){
-    return(x*2)
-  }else{
-    print("x is not numeric")
-  }
-}
